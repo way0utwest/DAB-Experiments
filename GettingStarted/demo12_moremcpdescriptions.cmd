a6de41bf635d63ddc65c65bb23765bae89687f81
way0utwest <sjones@sqlservercentral.com>
2026-07-07 15:44:46 -0600

    new demo with bad field descriptions

---
+++ GettingStarted/demo12_moremcpdescriptions.cmd
@@ -0,0 +1,3 @@
+dab update "OrderDetails" --config dab-northwind.json --fields.name UnitPrice  --fields.description "The price of 100 units of the product at the time of the order"
+dab update "OrderDetails" --config dab-northwind.json --fields.name Quantity  --fields.description "The number of boxes of 100 items in the order"
+dab update "OrderDetails" --config dab-northwind.json --fields.name Discount  --fields.description "The discount in dollars and cents applied to the order total"
