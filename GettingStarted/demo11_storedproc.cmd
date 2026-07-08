a89da9d81886a582a89a8c5b800540d8771df2ae
way0utwest <sjones@sqlservercentral.com>
2026-07-08 11:44:39 -0600

    added 2nd proc demo

--- GettingStarted/demo11_storedproc.cmd
+++ GettingStarted/demo11_storedproc.cmd
@@ -1,4 +1,5 @@
 dab add CustomerOrderHistory --config dab-northwind.json --source dbo.CustOrderHist --source.type "stored-procedure" --parameters.name "CustomerID" --parameters.required "true" --parameters.description "Customer PK" --permissions "anonymous:execute" --graphql.operation "query"
+dab add CustomerOrdersDetail --config dab-northwind.json --source dbo.CustOrdersDetail --source.type "stored-procedure" --parameters.name "OrderID" --parameters.required "true" --parameters.description "Order PK" --permissions "anonymous:execute" --graphql.operation "query"
 @echo "-----------------------------------------"
 @echo "Start the API with dab start dab-northwind.json"   
 @echo "-----------------------------------------"
